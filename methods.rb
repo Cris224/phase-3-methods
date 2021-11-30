c9a3dce78bcc2de3beedea636c75e2c42363777f
Cristofer <cristoferlorenzana@gmail.com>
2021-11-30 08:46:49 -0600

    commit tree 1864395b9319571cc6af01322e5512c5a1f64137

--- methods.rb
+++ methods.rb
@@ -1,1 +1,33 @@
 # Your code here!
+# function myFunction(param) {
+#   console.log("Running myFunction");
+#   return param + 1;
+# }
+
+def myFunction(param)
+    puts "Running myFunction";
+    param + 1;
+end
+
+def greet_programmer
+    puts "Hello, programmer!"
+end
+
+def greet name
+    puts "Hello, #{name}!"
+end
+
+def greet_with_default name = "programmer"
+    puts "Hello, #{name}!"
+end
+
+def add a,b
+    a + b
+end
+
+def halve c
+    if c.class != Integer
+        return nil
+    end
+    c/2
+end
